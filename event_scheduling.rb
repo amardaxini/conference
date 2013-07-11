649a2a1cfbeaae3959f3f4a2cab35592b412cfeb
amardaxini <amardaxini@gmail.com>
2013-07-11 12:37:50 +0530

    Initial commit

---
+++ event_scheduling.rb
@@ -0,0 +1,28 @@
+events=>{
+	"Writing Fast Tests Against Enterprise Rails"=> 60,
+	"Overdoing it in Python"=> 45,
+	"Lua for the Masses"=> 30,
+	"Ruby Errors from Mismatched Gem Versions"=> 45,
+	"Common Ruby Errors"=> 45,
+	"Rails for Python Developers"=>5,
+	"Communicating Over Distance"=> 60,
+	"Accounting-Driven Development"=> 45,
+	"Woah"=> 30,
+	"Sit Down and Write"=> 30,
+	"Pair Programming vs Noise"=> 45,
+	"Rails Magic"=> 60,
+	"Ruby on Rails: Why We Should Move On"=> 60,
+	"Clojure Ate Scala (on my project)"=> 45,
+	"Programming in the Boondocks of Seattle"=> 30,
+	"Ruby vs. Clojure for Back-End Development"=> 30,
+	"Ruby on Rails Legacy App Maintenance"=> 60,
+	"A World Without HackerNews"=> 30,
+	"User Interface CSS in Rails Apps"=> 30
+}	
+
+# Distribute Time
+values = events.values
+# Each track has roughly 180-180 session
+# Get Total Conference Time
+# Get Total Track total time % 360  and divide by 360 you will get remainder if it less than 90 minute
+# i.e minimum time Should be for nretworking will be 15 minute if we want less reduce remainder time
